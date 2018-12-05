acc879449c46159fe41cfe5bc5d23ed5d6df1146
cl <13546417565@163.com>
2018-12-05 15:52:10 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("your message is " + message);
+    }
+}
---
+++ src/MainApp.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[]args){
+//        Hello hello=new Hello();
+//        hello.setMessage("Spring");
+//        hello.getMessage();
+//        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+//        Hello hello=(Hello)context.getBean("hello");
+        ApplicationContext context=new FileSystemXmlApplicationContext("src/Beans.xml");
+        Hello hello=(Hello)context.getBean("hello");
+        hello.getMessage();
+
+    }
+}
