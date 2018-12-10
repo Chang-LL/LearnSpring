015df278eea3af1628dba76a70250670a4cf7149
cl <13546417565@163.com>
2018-12-10 11:07:49 +0800

    实现 Bean嵌套

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,15 +1,5 @@
 public class TextEditor {
     private SpellChecker spellChecker;
-    private String text;
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        System.out.println("Inside set Text");
-        this.text = text;
-    }
 
     public void spellCheck() {
         spellChecker.checkSpelling();
