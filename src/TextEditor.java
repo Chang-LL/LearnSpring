20d1559419a518c116316e7264063bbc09ce044f
cl <13546417565@163.com>
2018-12-10 23:13:03 +0800

    实现 Bean @Autowired 注释 去除set方法

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,17 +1,15 @@
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
-    private SpellChecker spellChecker;
-
     @Autowired
-    public void setSpellChecker(SpellChecker spellChecker) {
-        this.spellChecker = spellChecker;
-    }
+    private SpellChecker spellChecker;
 
     public SpellChecker getSpellChecker() {
         return spellChecker;
     }
-
+    public TextEditor() {
+        System.out.println("Inside TextEditor constructor." );
+    }
     public void spellCheck() {
         spellChecker.checkSpelling();
     }
