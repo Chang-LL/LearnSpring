1c13a6eb6532b79f21ef88c130f811012f3f26e5
cl <13546417565@163.com>
2018-12-10 21:23:45 +0800

    实现 Bean 自动装配ByType

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,13 +1,16 @@
 public class TextEditor {
     private SpellChecker spellChecker;
     private String name;
+
+    public TextEditor(SpellChecker spellChecker, String name) {
+        this.spellChecker = spellChecker;
+        this.name = name;
+    }
+
     public void spellCheck() {
         spellChecker.checkSpelling();
     }
 
-    public void setSpellChecker(SpellChecker spellChecker) {
-        this.spellChecker = spellChecker;
-    }
 
     public SpellChecker getSpellChecker() {
         return spellChecker;
