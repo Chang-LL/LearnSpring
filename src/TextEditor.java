6f69466922a56e886aa300af1bbe6adea3b1c91e
cl <13546417565@163.com>
2018-12-10 11:04:24 +0800

    实现 Bean通过p-namespace函数依赖注入

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,5 +1,15 @@
 public class TextEditor {
     private SpellChecker spellChecker;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        System.out.println("Inside set Text");
+        this.text = text;
+    }
 
     public void spellCheck() {
         spellChecker.checkSpelling();
